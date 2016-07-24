3049f6b8f6ddbe98a8ecc1587963727792253d36
Eric Krause <krause197@gmail.com>
2016-07-24 13:34:20 -0700

    create function pingPong in backend logic and also create frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,28 @@
-var average = function(weather, scenery, activities) {
-  return (weather + scenery + activities) / 3;
-};
-var locale;
-
-var questionare = function(average) {
-  if (average <=1.75) {
-    locale = "Camp Arifjan, Kuwait";
-    $("#kuwait-img").show();
-  } else if ((average > 1.75) && (average <= 2.25)) {
-    locale = "Baghdad, Iraq";
-    $("#baghdad-img").show();
-  } else if ((average > 2.25) && (average <= 3.25)) {
-    locale = "Erbil, Iraq";
-    $("#erbil-img").show();
-  } else {
-    locale = "Bagram Airfield, Afganistan";
-    $("#afganistan-img").show();
-  }
+var pingPong = function(number) {
+  var returnArray = [];
+  for (var index = 1; index <= number; index += 1) {
+    if (index % 15 === 0){
+      returnArray.push("pingpong");
+    } else if (index % 3 === 0) {
+      returnArray.push("ping");
+    } else if (index % 5 === 0){
+      returnArray.push("pong");
+    } else {
+      returnArray.push(index);
+    }
+ }
+  return returnArray;
 };
 
 
 
 $(document).ready(function() {
-  $("form#questionare").submit(function(event) {
+  $("form#number").submit(function(event) {
     event.preventDefault();
-    var name = $("input#name").val();
-    var home = $("input#home").val();
-    var age = parseInt($("input#age").val());
-    var weather = parseInt($("input:radio[name=weather]:checked").val());
-    var scenery = parseInt($("input:radio[name=scenery]:checked").val());
-    var activities = parseInt($("input:radio[name=activities]:checked").val());
-    var userAverage = average(weather, scenery, activities);
-    questionare(userAverage);
+    var number = parseInt($("input#numberInput").val());
+    var result = pingPong(number);
+    $(".returnArray").text(result);
 
-    $("#locationResult").show();
-    $(".name").text(name);
-    $(".location").text(locale);
-    $(".home").text(home);
+    $("#result").show();
   });
 });
